43db1728247c56fce1ea8ab98731ea07b5c26cb6
seta-minhhoang <minh.hoang@setacinq.vn>
2021-09-29 23:07:57 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,15 @@
 module.exports = {
-  purge: [],
+  purge: [
+    '*.html',
+    '*.js',
+  ],
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-      center: true
-    },
-    fontFamily: {
-      "body": "'Inter', sans-serif"
-    },
     extend: {
-      colors: {
-        "blue": "#71B8FA",
-        "violet": "#5E5CC2",
-        "secondary": "#1C2B45",
-        "primary": "#E5E5E5",
-        "background": "#F3F4F6"
-      },
-      backgroundImage: theme => ({
-        'conclusion': "url('public/images/video box.jpg')",
-        'banner': "url('public/images/Banner-area.png')",
-      })
+      fontFamily: {
+        'maven-pro': ['Maven Pro']
+      }
     },
   },
   variants: {
